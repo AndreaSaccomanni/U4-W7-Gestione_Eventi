d6d10aa6bd382196bf04355ecfafe7238fefbbbb
Andrea Saccomanni <Andrea Saccomanni>
2025-02-21 14:30:03 +0100

    first commit

---
+++ src/main/java/com/example/U4_W7_Gestione_Eventi/U4W7GestioneEventiApplication.java
@@ -0,0 +1,13 @@
+package com.example.U4_W7_Gestione_Eventi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U4W7GestioneEventiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U4W7GestioneEventiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/U4_W7_Gestione_Eventi/U4W7GestioneEventiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.U4_W7_Gestione_Eventi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U4W7GestioneEventiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
